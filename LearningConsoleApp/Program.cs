891f7170e4385c9f4e0a20e3df363f0d2deed68b
Quentin McRee <qmcree@gmail.com>
2023-07-03 23:56:22 -0500

    Initial commit.

---
+++ LearningConsoleApp/Program.cs
@@ -0,0 +1,6 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello this is a test.");
+Console.Beep();
+var name = Console.ReadLine();
+Console.WriteLine($"My other test from {name}.");
